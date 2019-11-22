47811256055b170318c34e468e6fce6bd243a8a4
Matthew Bodily <mattcbodily@gmail.com>
2019-11-22 16:12:27 -0700

    pokedex now displays caught pokemon

--- src/Components/Pokedex.js
+++ src/Components/Pokedex.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 const Pokedex = (props) => (
     <div className='pokedex'>
-        
+        {props.pokedex.map((element, i) => (
+            <div key={i} className='caught-pokemon'>
+                <img src={element.image} alt={element.name}/>
+                <p>{element.name}</p>
+            </div>
+        ))}
     </div>
 )
 
