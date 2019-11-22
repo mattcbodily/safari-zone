8a280332bbd6ebedf153f3d5948d85391ab1c3da
Matthew Bodily <mattcbodily@gmail.com>
2019-11-22 11:45:21 -0700

    created slide and bounce animations for pokemon sprites

--- src/Components/WildPokemon.js
+++ src/Components/WildPokemon.js
@@ -4,8 +4,8 @@ class WildPokemon extends Component {
     render(){
         const {pokemon, shinyNum} = this.props;
         return(
-            <div>
-                {pokemon.sprites ? <img src={shinyNum > 97 ? pokemon.sprites.front_shiny : pokemon.sprites.front_default} alt={pokemon.name}/> : null}
+            <div className='wild-pokemon'>
+                {pokemon.sprites ? <img src={shinyNum > 97 ? pokemon.sprites.front_shiny : pokemon.sprites.front_default} alt={pokemon.name} className='wild-pokemon-image'/> : null}
             </div>
         )
     }
