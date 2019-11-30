3558f3706b08f52278858c481fef3b00ef1577c1
Matthew Bodily <mattcbodily@gmail.com>
2019-11-29 23:01:38 -0800

    added an action for removing used items

--- src/redux/inventoryReducer.js
+++ src/redux/inventoryReducer.js
@@ -7,12 +7,20 @@ let initialState = {
 };
 
 const GET_INVENTORY = 'GET_INVENTORY';
+const USE_ITEM = 'USE_ITEM';
 
 export function getInventory(){
     const inventory = axios.get('/api/inventory').then(res => res.data).catch(err => err.message);
     return {
         type: GET_INVENTORY,
         payload: inventory
+    }
+}
+
+export function useItem(inventoryObj){
+    return {
+        type: USE_ITEM,
+        payload: inventoryObj
     }
 }
 
@@ -25,6 +33,9 @@ export default function inventoryReducer(state = initialState, action){
             return {...state, inventory: payload, loading: false};
         case GET_INVENTORY + '_REJECTED':
             return {...state, errorMessage: payload};
+        case USE_ITEM:
+            //adjust this to find and remove the used item from state
+            return state;
         default:
             return state;
     }
