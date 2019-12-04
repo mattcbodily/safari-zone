da82a05ed9e4d63e36ddb90454ba6a6c3fadaac8
Matthew Bodily <mattcbodily@gmail.com>
2019-12-04 16:18:56 -0700

    pokeshop now displays available items as well as available pokedollars

--- src/Components/PokeShop.js
+++ src/Components/PokeShop.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import shopItems from '../assets/store.json';
 
 const PokeShop = (props) => {
+    let availableBalance = props.inventory.find(element => element.category === 'money')
+
     return (
         <div className='pokeshop'>
-            
+            <p>Balance: {availableBalance.qty}</p>
+            {shopItems.map((item, i) => (
+                <div key={i} className='inventory-item'>
+                    <img src={item.image} alt={item.name}/>
+                    <span>{item.name}</span>
+                    <span>{item.price}</span>
+                </div>
+            ))}
         </div>
     )
 }
